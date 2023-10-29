968ae3cdfd0f65634afb3d52dec126a289e514bd
Santiago Sanchez Garcia <ssanga@outlook.com>
2023-10-29 10:22:27 +0100

    no consigo hacer funcionar un paquete mio ni con el ejemplo de chatgpt, no se si será la estructura de carpetas, pasando a otro tema

---
+++ src/03_modules-and-packages/using_mychatgprpackage.py
@@ -0,0 +1,5 @@
+# https://chat.openai.com/c/fb678d99-82d7-4be5-8a85-def9465e39dd
+# from mychatgptpackage.module import hello_word
+from mychatgptpackage.mychatgptpackage import module
+
+print(hello_word())
